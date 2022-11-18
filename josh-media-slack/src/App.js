75a235cfc81ade374786190968ba6b6daf5f75f1
jamar Arkson <arksonjamar@gmail.com>
2022-11-18 18:51:12 +0000

    frontend joshmedia

--- josh-media-slack/src/App.js
+++ josh-media-slack/src/App.js
@@ -7,25 +7,52 @@ const App = () => {
     <div className="main-wrap">
       <div className="detail-image">
         <div className="h1-p-btn-div">
-          <p>
-            <h1>
-              You want to target
-              <br />
-              developers right?
-            </h1>
-            If you want to target devlopers you are in the right place to post
-            your
+          <h1>
+            You want to target
             <br />
-            ads. DevAsk puts your brand in the faces of thousands of developers
-            to
-            <br />
-            boost your ad conversion.
-            <br />
-            <button>Get started</button>
-          </p>
+            <span>developers </span>right?
+          </h1>
+          If you want to target devlopers you are in the right place to post
+          your
+          <br />
+          ads. DevAsk puts your brand in the faces of thousands of developers to
+          <br />
+          boost your ad conversion.
+          <br />
+          <button>Get started</button>
         </div>
         <div className="Image-div">
           <img src={myImage} alt="Mentor-Image" />
+        </div>
+      </div>
+      <div className="mid-page">
+        <div>What can we do for you?</div>
+        <div className="mid-3boxes">
+          <div className="midbox">
+            <h4>Build brand awareness</h4>
+            <p>
+              Become popular in your niche and increase brand recognition,
+              trustworthiness, and credibility.
+            </p>
+          </div>
+          <div className="midbox">
+            <h4>Target & convert leads</h4>
+            <p>
+              Influence high-intent prospects searching for solutions during
+              their consideration phase.
+            </p>
+          </div>
+          <div className="midbox">
+            <h4>Increase your website traffic</h4>
+            <p>
+              Promote marketing content, drive traffic to your tech-related
+              website, and boost conversions.
+            </p>
+          </div>
+        </div>
+        <div>
+          {" "}
+          <button>Get started</button>
         </div>
       </div>
     </div>
