0a7483f6f6de54ca92b6ae7f1b5871cdc526cf6f
Adeoniye Adelu <adeluadeoniye14@gmail.com>
2022-11-19 11:45:14 +0100

    dev-Adeoniye

---
+++ src/CareerPage/Careers/Careers.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import classes from './Careers.module.css';
+import Hero from '../../CareersComp/Hero/Hero';
+import Content from '../../CareersComp/Content/Content';
+import OpenPOS from '../../CareersComp/OpenPOS/OpenPOS';
+
+function Careers  () {
+
+	return (
+		
+			<div className={classes.Careers}>
+				<Hero />
+
+				{/* Selection section */}
+
+				<div className={classes.selection}>
+					<div className={classes.selectbox}>
+						<select>
+							<option hidden selected disabled>
+								Choose Job Category
+							</option>
+							<option>Choose Job Category</option>
+							<option>Choose Job Category</option>
+						</select>
+						<select>
+							<option hidden selected disabled>
+								Choose Location{' '}
+							</option>
+							<option>Choose Job Category</option>
+							<option>Choose Job Category</option>
+						</select>
+						<button type='button'>Search</button>
+					</div>
+					<p>
+						We solve problems across all boarders of the globe, with our remote
+						work culture. join us today!
+					</p>
+				</div>
+
+				{/* Contents */}
+				<Content />
+
+				{/* open position */}
+
+				<section>
+					<OpenPOS />
+				</section>
+			</div>
+		
+	);
+};
+
+export default Careers;
