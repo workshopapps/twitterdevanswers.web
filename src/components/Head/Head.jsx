e5a8cfc987606dc7ece9314c1dc6e05dced58f31
amarapeace <onwunzoamara@yahoo.com>
2022-11-19 16:16:43 +0100

    bug fix try after recloning

--- src/components/Head/Head.jsx
+++ src/components/Head/Head.jsx
@@ -5,7 +5,7 @@ const Head = () => {
   return (
     <div className={styles.head}>
       <h1>Cookie Policy for DevAsk</h1>
-      <h5>This is the Cookie Policy for DevAsk, accessible from URL</h5>
+      <h5>This is the Cookie Policy for DevAsk, accessible from URL checking</h5>
     </div>
   );
 }
