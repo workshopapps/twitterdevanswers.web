1f8d7eb34548dfe9f9c024d6969a894bf7b6e5e5
MiracleUFO <miracle.ufodiama.242949@unn.edu.ng>
2022-11-19 11:12:32 +0100

    refactor: changed ternary to if statements for readability and styling tweaks

--- src/pages/Notifications/index.jsx
+++ src/pages/Notifications/index.jsx
@@ -45,29 +45,38 @@ const Notifications = () => {
           const displayName = notification?.from?.displayName || question?.from?.displayName;
           const avatar = notification?.from?.avatar || question?.from?.avatar;
 
-          const typeText =
-            type === 'like' ?
-              <span style={{fontWeight: 300}}>liked <span style={{fontWeight: 400}}>your Response</span></span>
-              : type === 'reply' ?
-                <span>Responded to <a href="#"><b>your Question</b></a></span>
-              : type === 'wallet-add' ?
-                  <span style={{ display: 'flex', alignItems: 'center' }}>
-                    <img src={CoinIcon} alt="Coin icon" style={{ marginRight: 10 }} />
-                    <span style={{fontWeight: 300}}><span style={{ fontWeight: 600 }}>{amount}ETH</span> added to your <span style={{fontWeight: 400}}>Wallet</span></span>
-                  </span>
-                : type === 'wallet-remove' ?
-                    <span style={{ display: 'flex', alignItems: 'center' }}>
-                      <img src={CoinIcon} alt="Coin icon" style={{ marginRight: 10 }} />
-                      <span style={{fontWeight: 300}}><span style={{ fontWeight: 600 }}>{amount}ETH</span> removed from your <b>Wallet</b></span>
-                    </span>
-                  : type === 'for-you' && !question && tag ?
-                      <span style={{ display: 'flex', alignItems: 'center' }}>
-                        {tag ? <img src={TagIcon} alt="tag icon" /> : null}
-                        <span>&nbsp;{title} {tag ? <b>{`on ${tag}`}</b>: null}</span>
-                      </span>
-                    : type === 'for-you' && question ?
-                        <p>asked a question</p>
-                      : null;
+          let typeText = ''; // What to display based on the notification type
+
+          if (type === 'like') {
+            typeText =  <span style={{fontWeight: 300}}>liked <span style={{fontWeight: 400}}>your Response</span></span>;
+          }
+          if (type === 'reply') {
+            typeText = <span>Responded to <b>your Question</b></span>;
+          }
+          if (type === 'wallet-add') {
+            typeText =  
+            <span style={{ display: 'flex', alignItems: 'center' }}>
+              <img src={CoinIcon} alt="Coin icon" style={{ marginRight: 10 }} />
+              <span style={{fontWeight: 300}}><span style={{ fontWeight: 600 }}>{amount}ETH</span> added to your <span style={{fontWeight: 400}}>Wallet</span></span>
+            </span>
+          }
+          if (type === 'wallet-remove') {
+            typeText = 
+            <span style={{ display: 'flex', alignItems: 'center' }}>
+              <img src={CoinIcon} alt="Coin icon" style={{ marginRight: 10 }} />
+              <span style={{fontWeight: 300}}><span style={{ fontWeight: 600 }}>{amount}ETH</span> removed from your <span style={{fontWeight: 400}}>Wallet</span></span>
+            </span>
+          }
+          if (type === 'for-you' && !question && tag) {
+            typeText =
+            <span style={{ display: 'flex', alignItems: 'center' }}>
+              {tag ? <img src={TagIcon} alt="tag icon" /> : null}
+              <span>&nbsp;{title} {tag ? <b>{`on ${tag}`}</b>: null}</span>
+            </span>
+          }
+          if (type === 'for-you' && question) {
+            typeText = <p>asked a question</p>;
+          }
           
           return (
             <div key={i} className={`${styles.notification_item} ${unread ? styles.unread : ''}`}>
