899b8643c3641cdf5a12fc2fae3a3f973cdedd68
Mejor <amujobabatunde7@gmail.com>
2022-11-19 18:48:36 +0100

    feat: adjusted the file structure to solve conflicting files

--- twitterdevanswers.web/src/App.js
+++ twitterdevanswers.web/src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import UserPage from './pages/userPage/usePage';
-import UserProfileCard from './component/UserProfileCard';
+ import UserPage from './user-page/pages/userPage/usePage';
+// import UserProfileCard from './component/UserProfileCard';
 
 function App() {
   return (
     <div className="App">
-      <UserPage />
-     
+
+<UserPage/>
     </div>
   );
 }
