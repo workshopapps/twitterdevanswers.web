c32d4f1b6a59968ef11c7374dc98565b9fd2a25f
nitoc <daren2track@gmail.con>
2022-11-20 03:58:05 +0100

    feat: add profile page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+import React from 'react';
 // import Notifications from './pages/Notifications';
-import Profile from './pages/Profile/index';
+import Profile from './pages/Profile';
 import './App.css';
 
 function App() {
