a5664ec9bc524ba357199997462f260b2d997cca
amarapeace <onwunzoamara@yahoo.com>
2022-11-20 16:31:26 +0100

    feat: code add for page index

---
+++ src/pages/CookiePolicy/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import CookieHead from '../../components/CookieHead/CookieHead';
+// import CookieBody from '../../components/CookieBody/CookieBody';
+
+function CookiePolicy() {
+	return (
+		<div>
+			{/* <CookieHead />
+			<CookieBody /> */}
+		</div>
+	);
+}
+
+export default CookiePolicy;
