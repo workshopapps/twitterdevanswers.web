d43149a3d70a9ee31519e5417e7aa281afe0f8bf
Mejor <amujobabatunde7@gmail.com>
2022-11-20 22:13:01 +0100

    implemented the user page design

---
+++ src/pages/UserPage/users.jsx
@@ -0,0 +1,196 @@
+ const users = [
+    {
+        img: '../assests/userPageAssets/Ellipse0.png',
+        userName: 'Ada Stevens',
+        userStack: 'Senior Product Designer',
+        followers: '1570',
+        userFavoriteLanguage:'sql, oracle, windows-options'
+    },
+    {
+        img: '../assests/userPageAssets/Ellipse1.png',
+        userName: 'Ada Stevens',
+        userStack: 'Senior Product Designer',
+        followers: '1570',
+        userFavoriteLanguage:'sql, oracle, windows-options'
+    },
+    {
+        img: '../assests/userPageAssets/Ellipse2.png',
+        userName: 'Ada Stevens',
+        userStack: 'Senior Product Designer',
+        followers: '1570',
+        userFavoriteLanguage:'sql, oracle, windows-options'
+    },
+    {
+        img: '../assests/userPageAssets/Ellipse3.png',
+        userName: 'Ada Stevens',
+        userStack: 'Senior Product Designer',
+        followers: '1570',
+        userFavoriteLanguage:'sql, oracle, windows-options'
+    },
+    {
+        img: '../assests/userPageAssets/Ellipse4.png',
+        userName: 'Ada Stevens',
+        userStack: 'Senior Product Designer',
+        followers: '1570',
+        userFavoriteLanguage: 'sql, oracle, windows-options'
+    },
+    {
+        img: '../assests/userPageAssets/Ellipse5.png',
+        userName: 'Ada Stevens',
+        userStack: 'Senior Product Designer',
+        followers: '1570',
+        userFavoriteLanguage: 'sql, oracle, windows-options'
+    },
+    {
+        img: '../assests/userPageAssets/Ellipse6.png',
+        userName: 'Ada Stevens',
+        userStack: 'Senior Product Designer',
+        followers: '1570',
+        userFavoriteLanguage: 'sql, oracle, windows-options'
+    },
+    {
+        img: '../assests/userPageAssets/Ellipse7.png',
+        userName: 'Ada Stevens',
+        userStack: 'Senior Product Designer',
+        followers: '1570',
+        userFavoriteLanguage: 'sql, oracle, windows-options'
+    },
+    {
+        img: '../assests/userPageAssets/Ellipse8.png',
+        userName: 'Ada Stevens',
+        userStack: 'Senior Product Designer',
+        followers: '1570',
+        userFavoriteLanguage: 'sql, oracle, windows-options'
+    },
+    {
+        img: '../assests/userPageAssets/Ellipse16.png',
+        userName: 'Ada Stevens',
+        userStack: 'Senior Product Designer',
+        followers: '1570',
+        userFavoriteLanguage: 'sql, oracle, windows-options'
+    },
+    {
+        img: '../assests/userPageAssets/Ellipse11.png',
+        userName: 'Ada Stevens',
+        userStack: 'Senior Product Designer',
+        followers: '1570',
+        userFavoriteLanguage: 'sql, oracle, windows-options'
+    },
+    {
+        img: '../assests/userPageAssets/Ellipse10.png',
+        userName: 'Ada Stevens',
+        userStack: 'Senior Product Designer',
+        followers: '1570',
+        userFavoriteLanguage: 'sql, oracle, windows-options'
+    },
+    {
+        img: '../assests/userPageAssets/Ellipse12.png',
+        userName: 'Ada Stevens',
+        userStack: 'Senior Product Designer',
+        followers: '1570',
+        userFavoriteLanguage: 'sql, oracle, windows-options'
+    },
+    {
+        img: '../assests/userPageAssets/Ellipse1.png',
+        userName: 'Ada Stevens',
+        userStack: 'Senior Product Designer',
+        followers: '1570',
+        userFavoriteLanguage: 'sql, oracle, windows-options'
+    },
+    {
+        img: '../assests/userPageAssets/Ellipse5.png',
+        userName: 'Ada Stevens',
+        userStack: 'Senior Product Designer',
+        followers: '1570',
+        userFavoriteLanguage: 'sql, oracle, windows-options'
+    },
+    {
+        img: '../assests/userPageAssets/Ellipse11.png',
+        userName: 'Ada Stevens',
+        userStack: 'Senior Product Designer',
+        followers: '1570',
+        userFavoriteLanguage: 'sql, oracle, windows-options'
+    },
+    {
+        img: '../assests/userPageAssets/Ellipse15.png',
+        userName: 'Ada Stevens',
+        userStack: 'Senior Product Designer',
+        followers: '1570',
+        userFavoriteLanguage: 'sql, oracle, windows-options'
+    },
+    {
+        img: '../asseests/userPageAssets/Ellipse14.png',
+        userName: 'Ada Stevens',
+        userStack: 'Senior Product Designer',
+        followers: '1570',
+        userFavoriteLanguage: 'sql, oracle, windows-options'
+    },
+    {
+        img: '../asseests/userPageAssets/Ellipse10.png',
+        userName: 'Ada Stevens',
+        userStack: 'Senior Product Designer',
+        followers: '1570',
+        userFavoriteLanguage: 'sql, oracle, windows-options'
+    },
+    {
+        img: '../asseests/userPageAssets/Ellipse17.png',
+        userName: 'Ada Stevens',
+        userStack: 'Senior Product Designer',
+        followers: '1570',
+        userFavoriteLanguage: 'sql, oracle, windows-options'
+    },
+    {
+        img: '../assests/userPageAssets/Ellipse7.png',
+        userName: 'Ada Stevens',
+        userStack: 'Senior Product Designer',
+        followers: '1570',
+        userFavoriteLanguage: 'sql, oracle, windows-options'
+    },
+    {
+        img: '../assests/userPageAssets/Ellipse14.png',
+        userName: 'Ada Stevens',
+        userStack: 'Senior Product Designer',
+        followers: '1570',
+        userFavoriteLanguage: 'sql, oracle, windows-options'
+    },
+    {
+        img: '../assests/userPageAssets/Ellipse1.png',
+        userName: 'Ada Stevens',
+        userStack: 'Senior Product Designer',
+        followers: '1570',
+        userFavoriteLanguage: 'sql, oracle, windows-options'
+    },
+    {
+        img: '../assests/userPageAssets/Ellipse3.png',
+        userName: 'Ada Stevens',
+        userStack: 'Senior Product Designer',
+        followers: '1570',
+        userFavoriteLanguage: 'sql, oracle, windows-options'
+    },
+
+    {
+        img: '../assests/userPageAssets/Ellipse21.png',
+        userName: 'Ada Stevens',
+        userStack: 'Senior Product Designer',
+        followers: '1570',
+        userFavoriteLanguage: 'sql, oracle, windows-options'
+    },
+    {
+        img: '../assests/userPageAssets/Ellipse19.png',
+        userName: 'Ada Stevens',
+        userStack: 'Senior Product Designer',
+        followers: '1570',
+        userFavoriteLanguage: 'sql, oracle, windows-options'
+    },
+    
+    {
+        img: '../assests/userPageAssets/Ellipse20.png',
+        userName: 'Ada Stevens',
+        userStack: 'Senior Product Designer',
+        followers: '1570',
+        userFavoriteLanguage: 'sql, oracle, windows-options'
+    },
+    
+   
+]
+export default users
