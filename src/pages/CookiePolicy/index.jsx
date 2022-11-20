64ff0dab32791cd998d8e634220e8bb879a6ffeb
amarapeace <onwunzoamara@yahoo.com>
2022-11-20 17:00:41 +0100

    :hammer: import and link between cookiepolicy page and cookiebody component

--- src/pages/CookiePolicy/index.jsx
+++ src/pages/CookiePolicy/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import CookieHead from '../../components/CookieHead/CookieHead';
-// import CookieBody from '../../components/CookieBody/CookieBody';
+import CookieBody from '../../components/CookieBody/CookieBody';
 
 function CookiePolicy() {
 	return (
 		<div>
 			<CookieHead />
-			{/* <CookieBody /> */}
+			<CookieBody />
 		</div>
 	);
 }
