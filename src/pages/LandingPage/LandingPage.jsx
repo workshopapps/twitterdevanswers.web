100c9519d5d0aca8d6da0781c38aca7d453f7f8c
BamBam-sys <lawalayobamidavid@gmail.com>
2022-11-20 17:13:27 +0100

    feat: added cta and newsletter sub section

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Clients from '../../components/Clients/Clients';
-// import Cta from '../../components/Cta/Cta';
+import Cta from '../../components/Cta/Cta';
 import Hero from '../../components/Hero/Hero';
 import HowItWorks from '../../components/HowItWorks/HowItWorks';
-// import NewsletterSub from '../../components/NewsletterSub/NewsletterSub';
-// import Footer from './../../components/Footer/Footer';
+import NewsletterSub from '../../components/NewsletterSub/NewsletterSub';
+import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 
 function LandingPage() {
@@ -14,9 +14,9 @@ function LandingPage() {
 			<Hero />
 			<Clients />
 			<HowItWorks />
-			{/* <Cta /> */}
-			{/* <NewsletterSub /> */}
-			{/* <Footer /> */}
+			<Cta />
+			<NewsletterSub />
+			<Footer />
 		</>
 	);
 }
