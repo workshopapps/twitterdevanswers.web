c1b5eca6e09229eeb673a67c8acfa373e779cd60
BamBam-sys <lawalayobamidavid@gmail.com>
2022-11-20 15:40:05 +0100

    added internal header and footer

--- src/components/InternalFooter/InternalFooter.jsx
+++ src/components/InternalFooter/InternalFooter.jsx
@@ -8,48 +8,48 @@ import { ReactComponent as SettingsIcon } from '../../assets/settings.svg';
 import styles from './internalFooter.module.css';
 
 export default function InternalFooter() {
-  // footer component for internal pages. only avaible on mobile screens
+	// footer component for internal pages. only avaible on mobile screens
 
-  const activeStyle = ({ isActive }) =>
-    isActive ? styles.isActive : undefined;
+	const activeStyle = ({ isActive }) =>
+		isActive ? styles.isActive : undefined;
 
-  const linkStyle = {
-    textDecoration: 'none',
-    fontWeight: '500',
-    fontSize: '18px',
-    lineHeight: '28px',
-    color: '#4343de',
-  };
+	const linkStyle = {
+		textDecoration: 'none',
+		fontWeight: '500',
+		fontSize: '18px',
+		lineHeight: '28px',
+		color: '#4343de',
+	};
 
-  return (
-    <div className={styles.footerContainer}>
-      <ul className={`${styles.footer} lpContainer`}>
-        <li>
-          <NavLink to="/" style={linkStyle} className={activeStyle}>
-            <HomeIcon className={styles.icon} />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/tags-page" style={linkStyle} className={activeStyle}>
-            <TagIcon className={styles.icon} />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink style={linkStyle} to="/users-page" className={activeStyle}>
-            <UsersIcon className={styles.icon} />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/wallet" style={linkStyle} className={activeStyle}>
-            <WalletIcon className={styles.icon} />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/settings" style={linkStyle} className={activeStyle}>
-            <SettingsIcon className={styles.icon} />
-          </NavLink>
-        </li>
-      </ul>
-    </div>
-  );
+	return (
+		<div className={styles.footerContainer}>
+			<ul className={`${styles.footer} lpContainer`}>
+				<li>
+					<NavLink to="/" style={linkStyle} className={activeStyle}>
+						<HomeIcon className={styles.icon} />
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to="/tags-page" style={linkStyle} className={activeStyle}>
+						<TagIcon className={styles.icon} />
+					</NavLink>
+				</li>
+				<li>
+					<NavLink style={linkStyle} to="/users-page" className={activeStyle}>
+						<UsersIcon className={styles.icon} />
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to="/wallet" style={linkStyle} className={activeStyle}>
+						<WalletIcon className={styles.icon} />
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to="/settings" style={linkStyle} className={activeStyle}>
+						<SettingsIcon className={styles.icon} />
+					</NavLink>
+				</li>
+			</ul>
+		</div>
+	);
 }
--- src/components/InternalHeader/InternalHeader.jsx
+++ src/components/InternalHeader/InternalHeader.jsx
@@ -13,98 +13,98 @@ import brandLogo from '../../assets/brand-logo.svg';
 import styles from './internalHeader.module.css';
 
 export default function InternalHeader() {
-  //  header component for internal pages
+	//  header component for internal pages
 
-  const activeStyle = ({ isActive }) =>
-    isActive ? styles.isActive : undefined;
+	const activeStyle = ({ isActive }) =>
+		isActive ? styles.isActive : undefined;
 
-  const linkStyle = {
-    textDecoration: 'none',
-    fontWeight: '500',
-    fontSize: '18px',
-    lineHeight: '28px',
-    color: '#4343de',
-  };
+	const linkStyle = {
+		textDecoration: 'none',
+		fontWeight: '500',
+		fontSize: '18px',
+		lineHeight: '28px',
+		color: '#4343de',
+	};
 
-  return (
-    <div className={styles.headerContainer}>
-      <div className={`${styles.header} lpContainer`}>
-        <div className={styles.left}>
-          <Link to="/" className={styles.brand}>
-            <img src={brandLogo} alt="brand logo" />
-            <span>DevAsk</span>
-          </Link>
-          <div className={styles.search}>
-            <SearchIcon />
-            <input type="text" placeholder="Search questions..." />
-          </div>
-        </div>
+	return (
+		<div className={styles.headerContainer}>
+			<div className={`${styles.header} lpContainer`}>
+				<div className={styles.left}>
+					<Link to="/" className={styles.brand}>
+						<img src={brandLogo} alt="brand logo" />
+						<span>DevAsk</span>
+					</Link>
+					<div className={styles.search}>
+						<SearchIcon />
+						<input type="text" placeholder="Search questions..." />
+					</div>
+				</div>
 
-        <div className={styles.right}>
-          <ul className={styles.links}>
-            <li>
-              <NavLink to="/" style={linkStyle} className={activeStyle}>
-                <div className={styles.link}>
-                  <HomeIcon className={styles.icon} />
-                  <span>Home</span>
-                </div>
-              </NavLink>
-            </li>
+				<div className={styles.right}>
+					<ul className={styles.links}>
+						<li>
+							<NavLink to="/" style={linkStyle} className={activeStyle}>
+								<div className={styles.link}>
+									<HomeIcon className={styles.icon} />
+									<span>Home</span>
+								</div>
+							</NavLink>
+						</li>
 
-            <li>
-              <NavLink
-                to="/tags-page"
-                style={linkStyle}
-                className={activeStyle}
-              >
-                <div className={styles.link}>
-                  <TagIcon className={styles.icon} /> <span>Tag</span>
-                </div>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                style={linkStyle}
-                to="/users-page"
-                className={activeStyle}
-              >
-                <div className={styles.link}>
-                  <UsersIcon className={styles.icon} /> <span>Users</span>
-                </div>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/wallet" style={linkStyle} className={activeStyle}>
-                <div className={styles.link}>
-                  <WalletIcon className={styles.icon} /> <span>Wallet</span>
-                </div>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/settings" style={linkStyle} className={activeStyle}>
-                <div className={styles.link}>
-                  <SettingsIcon className={styles.icon} /> <span>Settings</span>
-                </div>
-              </NavLink>
-            </li>
-          </ul>
-          <div className={styles.rest}>
-            <NavLink to="/notifications-page" className={activeStyle}>
-              <NotificationIcon className={styles.icon} />
-            </NavLink>
-            <SortIcon className={styles.sortIcon} />
-            <div className={styles.user}>
-              <div className={styles.avatar}>
-                <img src={avatar} alt="avatar" />
-              </div>
-              <div className={styles.nameStatus}>
-                <p>Kayla Nicole</p>
-                <span>Online</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+						<li>
+							<NavLink
+								to="/tags-page"
+								style={linkStyle}
+								className={activeStyle}
+							>
+								<div className={styles.link}>
+									<TagIcon className={styles.icon} /> <span>Tag</span>
+								</div>
+							</NavLink>
+						</li>
+						<li>
+							<NavLink
+								style={linkStyle}
+								to="/users-page"
+								className={activeStyle}
+							>
+								<div className={styles.link}>
+									<UsersIcon className={styles.icon} /> <span>Users</span>
+								</div>
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to="/wallet" style={linkStyle} className={activeStyle}>
+								<div className={styles.link}>
+									<WalletIcon className={styles.icon} /> <span>Wallet</span>
+								</div>
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to="/settings" style={linkStyle} className={activeStyle}>
+								<div className={styles.link}>
+									<SettingsIcon className={styles.icon} /> <span>Settings</span>
+								</div>
+							</NavLink>
+						</li>
+					</ul>
+					<div className={styles.rest}>
+						<NavLink to="/notifications-page" className={activeStyle}>
+							<NotificationIcon className={styles.icon} />
+						</NavLink>
+						<SortIcon className={styles.sortIcon} />
+						<div className={styles.user}>
+							<div className={styles.avatar}>
+								<img src={avatar} alt="avatar" />
+							</div>
+							<div className={styles.nameStatus}>
+								<p>Kayla Nicole</p>
+								<span>Online</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
