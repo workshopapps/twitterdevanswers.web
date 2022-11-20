51feddffccb753752ed57dc9ef83b7e78fcf9cdc
BamBam-sys <lawalayobamidavid@gmail.com>
2022-11-20 16:57:17 +0100

    feat: added hero and clients section

---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Clients from '../../components/Clients/Clients';
+// import Cta from '../../components/Cta/Cta';
+import Hero from '../../components/Hero/Hero';
+// import HowItWorks from '../../components/HowItWorks/HowItWorks';
+// import NewsletterSub from '../../components/NewsletterSub/NewsletterSub';
+// import Footer from './../../components/Footer/Footer';
+import Header from '../../components/Header/Header';
+
+function LandingPage() {
+	return (
+		<>
+			<Header />
+			<Hero />
+			<Clients />
+			{/* <HowItWorks /> */}
+			{/* <Cta /> */}
+			{/* <NewsletterSub /> */}
+			{/* <Footer /> */}
+		</>
+	);
+}
+
+export default LandingPage;
