3b0ee8e6cefb705744aa88d68a1ee980ef53f018
codedlibra <mrcoded98@gmail.com>
2022-11-25 23:46:35 -0800

    feat: implemented contextAPI on login page

--- src/pages/AuthPage/Login/index.jsx
+++ src/pages/AuthPage/Login/index.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState, useContext} from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import styles from './styles.module.css';
 import Googleicon from '../../../assets/google.svg';
 import GithubIcon from '../../../assets/github.svg';
 import Microsoficon from '../../../assets/microsoft.svg';
 import AuthPage from '..';
+import { AppContext } from '../../../store/AppContext';
+import { USER_LOGGED_IN } from '../../../store/actionTypes';
 
 
 const signInOptions = [
@@ -50,11 +52,11 @@ function InputCheckbox() {
 				className={styles.input__checkbox}
 				id="keep-logged-in"
 			/>
-			<label aria-controls='label'
+			<label
 				htmlFor="keep-logged-in"
 				className={styles['form-group__checkbox-label']}
 			>
-				<input/>
+				<input />
 				Keep me logged in
 			</label>
 		</div>
@@ -62,6 +64,39 @@ function InputCheckbox() {
 }
 
 function Login() {
+	
+	const [input, setInput] = useState({
+		password: '',
+		email: '',
+	});
+
+	const { dispatch } = useContext(AppContext);
+	const navigate = useNavigate();
+
+	const changeHandler = (event) => {
+		setInput((prev) => ({
+			...prev,
+			[event.target.name]: event.target.value,
+		}));
+	};
+
+	const handleLogIn = async (event) => {
+		event.preventDefault();
+		try {
+			localStorage.setItem('user', JSON.stringify(input));
+
+			dispatch({
+				type: USER_LOGGED_IN,
+				payload: input,
+			});
+
+			navigate('/');
+			window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+		} catch (error) {
+			throw new Error(error);
+		}
+	};
+
 	return (
 		<AuthPage
 			inputs={inputs}
@@ -70,6 +105,8 @@ function Login() {
 			authAltText="Or Log in with"
 			inputCheckbox={<InputCheckbox />}
 			buttonLabel="Login"
+			onChange={changeHandler}
+			onSubmit={handleLogIn}
 		>
 			<p className={styles['alt-auth']}>
 				Don&apos;t have an account?{' '}
