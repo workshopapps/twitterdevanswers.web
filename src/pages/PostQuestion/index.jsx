ef55f8d1050f01c48bb117328e0d869ebc483b96
Praise Obende <davidobende@gmail.com>
2022-12-04 05:21:40 +0100

    feat: FE-107: Updated the api endpoint url to api.devask.hng.tech

--- src/pages/PostQuestion/index.jsx
+++ src/pages/PostQuestion/index.jsx
@@ -213,7 +213,7 @@ function PostQuestion() {
 
 		try {
 			const data = await axios.post(
-				'https://pacific-peak-54505.herokuapp.com/questions',
+				'https://api.devask.hng.tech/questions',
 				details,
 				{
 					headers,
@@ -363,7 +363,7 @@ function PostQuestion() {
 								onChange={handleChange}
 								required
 								onBlur={handleNextDetail}
-								autoComplete
+								autoComplete="true"
 							/>
 						</div>
 
