65131995cd20dec5ee18553d27ffea0f5535b70f
Ola-Steeve <64215032+Ola-Steeve@users.noreply.github.com>
2022-12-09 14:34:26 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import * as atatus from 'atatus-js';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+atatus.config('48933a5ad3684b9d9351f3b103e77b8f').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
