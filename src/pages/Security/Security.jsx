fb55295869fb7cbb7df66321180e428b2d2d8ed9
Nnaemeka Joseph <101423256+nnaemekaxi@users.noreply.github.com>
2022-12-09 15:13:33 +0100

    2fa functionality added

--- src/pages/Security/Security.jsx
+++ src/pages/Security/Security.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import styles from "./Security.module.css";
 import account from "../../assets/security-images/account.png";
 import security from "../../assets/security-images/security.png";
@@ -7,23 +8,58 @@ import notification from "../../assets/security-images/notification.png";
 import appearance from "../../assets/security-images/appearance.png"
 
 function Security() {
-    const [isEnabled, setIsEnabled] = useState(JSON.parse(localStorage.getItem("Enable")) || false);
-    const [isVerified, setIsVerified] = useState(JSON.parse(localStorage.getItem("Verify")) || false);
+    const [getEmail, setGetEmail] = useState("email");
+    const [postEmail, setPostEmail] = useState("email");
+    const [OTP, setOTP] = useState("OTP");
+    const [isVisible, setIsVisible] = useState(false);
+    const [isClicked, setIsClicked] = useState(false);
+    const [response, setResponse] = useState("")
 
-    useEffect(() => {
-        localStorage.setItem("Enable", JSON.stringify(isEnabled))
-    }, [isEnabled]);
-    useEffect(() => {
-        localStorage.setItem("Verify", JSON.stringify(isVerified))
-    }, [isVerified]);
 
-        const handleEnable = () => {
-                setIsEnabled(!isEnabled);  
-                
+
+    function toggleForm(){
+        setIsVisible(!isVisible);
+    }
+    function revealMail(){
+        if (getEmail === "email" || getEmail === ""){
+            setIsClicked(false)
+        } else{
+            setIsClicked(!isClicked);
         }
-        const handleIsVerified = () => {
-            setIsVerified(!isVerified);
+
+    }
+        const handleSubmit = async (e) => {
+            e.preventDefault();
+            const emailAddress = JSON.stringify(getEmail);
+            try {
+                const { data } = await axios.put('https://api.devask.hng.tech/auth/setup-mfa', {
+                    "email": emailAddress
+                });
+                if(data){
+                    setResponse("Email Sent");
+                };
+            } catch (error) {
+                setResponse("Could not send request. Please try again!");
+            }
         }
+        
+            const handleSubmitOTP = async (e) => {
+                e.preventDefault();
+                const emailAddress = JSON.stringify(postEmail);
+                const myOTP = JSON.stringify(OTP);
+                try {
+                    const { data } = await axios.post('https://api.devask.hng.tech/auth/validate-mfa', {
+                            email: emailAddress,
+                            mfa_hash: myOTP
+                    });
+                    if(data){
+                        setResponse("Validation Successful");
+                    }; 
+                } catch (error) {
+                    setResponse("Validation Unsuccessful. Please try again!");
+    
+                } 
+            }
 
 
     return(
@@ -89,14 +125,30 @@ function Security() {
                         <h1>Two-Factor Authentication</h1>
                         <p>Enabling Two-factor Authentication help protect your account better by sending you OTP on each login</p>
                     </section>
-                    <button type="submit" onClick={handleEnable} > { isEnabled ? "Disable": "Enable" }</button>
+                    <button type="submit" className={styles.handleEnable} onClick={toggleForm}>Enable</button>
                 </div>
+                {isVisible && (
+                <div className={styles.TwoFactorAuthForms} id="two-factor-forms">
+                    <p>Please fill the required fields</p>
+                <form className={styles.GetTwoAuthForm} onSubmit={handleSubmit} method="POST">
+                        <input type="email" name="email" required value={getEmail} onChange={(e) => setGetEmail(e.target.value)} />
+                        <button type="submit" onClick={revealMail}>Get Code</button>
+                    </form>
+                    {isClicked && <p>Please Check your email and enter the OTP</p>}
+                    <form className={styles.GetTwoAuthForm} onSubmit={handleSubmitOTP} method="POST">
+                        <input type="email" name="email" required value={postEmail} onChange={(e) => setPostEmail(e.target.value)} />
+                        <input type="text" name="OTP" required value={OTP} onChange={(e) => setOTP(e.target.value)} />
+                        <button type="submit">Send Code</button>
+                    </form>
+                            <p>{response}</p>
+                </div>
+                )}
                 <div className={styles.VerifyEmail}>
                     <section>
                         <h1>Verify E-mail</h1>
                         <p>Confirmation email has been sent to the e-mail address provided, check mail to confirm</p>
                     </section>
-                    <button type="submit" onClick={handleIsVerified} > { isVerified ? "Verified": "Verify" }</button>
+                    <button type="submit"> Verify</button>
                 </div>
                 <form className={styles.SecurityForm}>
                     <section className={styles.WalletSection}>
