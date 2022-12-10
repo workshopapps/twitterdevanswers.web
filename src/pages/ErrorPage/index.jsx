db8037f2db0425a75bd478993c3a73e58d912fd8
theFrontrade <sadeniyi016@gmail.com>
2022-12-10 22:31:44 +0100

    Contact page, Error Page and About page fix

--- src/pages/ErrorPage/index.jsx
+++ src/pages/ErrorPage/index.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import errorpage from '../../assets/error-images/errorpage.JPG';
+import navigate from '../../assets/error-images/navigate.svg';
 import styles from './styles.module.css';
 
 export default function ErrorPage() {
-	const navigate = useNavigate();
+
 	return (
-		<div>
+		<div className={styles.errorWrap}>
 			<img src={errorpage} className={styles.error} alt="404" />
-			<button
-				type="button"
-				onClick={() => navigate(-1)}
-				style={{ margin: '10px' }}
-			>
-				Go Back
-			</button>
-			<br />
+			<p>Oops! We couldnt find that page</p>
+			<span>This page does not exist. Maybe you can find what you need here instead</span>
+			<img src={navigate} className={styles.navigate} alt="404" />
+			<Link to="/" className={styles.button}>
+				Home
+			</Link>
+			
+			
+			
+		
 		</div>
 	);
 }
