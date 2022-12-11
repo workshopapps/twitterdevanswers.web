2696635b871810f8db517a2b8d1f5d83bdf04949
devbayo <ibrahimabdulqoyyum@gmail.com>
2022-12-11 14:48:55 +0100

    feat: added side bar

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -19,10 +19,120 @@ function Profile() {
 			</main>
 			<aside className={styles.aside}>
 				<section className={styles['relevant-topics']}>
-					<h3 className={styles['heading-secondary']}>You might like</h3>
+					{/* Topics suggestions */}
+					<div className={`${styles.topics} ${styles['aside-container']}`}>
+						<h3 className={styles['heading-secondary']}>You might like</h3>
 
-					<div className={styles.topics}>
-						<div className={styles.topic}>Hello</div>
+						<div className={styles.topic}>
+							<img
+								src="https://www.pngitem.com/pimgs/m/581-5813504_avatar-dummy-png-transparent-png.png"
+								alt="user avatar"
+							/>
+							<div className={styles.content}>
+								<h4>
+									How to get first item on screen of a list view while scrolling
+								</h4>
+								<p>12 Replies</p>
+							</div>
+						</div>
+						<div className={styles.topic}>
+							<img
+								src="https://www.pngitem.com/pimgs/m/581-5813504_avatar-dummy-png-transparent-png.png"
+								alt="user avatar"
+							/>
+							<div className={styles.content}>
+								<h4>
+									How to get first item on screen of a list view while scrolling
+								</h4>
+								<p>12 Replies</p>
+							</div>
+						</div>
+						<div className={styles.topic}>
+							<img
+								src="https://www.pngitem.com/pimgs/m/581-5813504_avatar-dummy-png-transparent-png.png"
+								alt="user avatar"
+							/>
+							<div className={styles.content}>
+								<h4>
+									How to get first item on screen of a list view while scrolling
+								</h4>
+								<p>12 Replies</p>
+							</div>
+						</div>
+						<div className={styles.topic}>
+							<img
+								src="https://www.pngitem.com/pimgs/m/581-5813504_avatar-dummy-png-transparent-png.png"
+								alt="user avatar"
+							/>
+							<div className={styles.content}>
+								<h4>
+									How to get first item on screen of a list view while scrolling
+								</h4>
+								<p>12 Replies</p>
+							</div>
+						</div>
+						<div className={styles.topic}>
+							<img
+								src="https://www.pngitem.com/pimgs/m/581-5813504_avatar-dummy-png-transparent-png.png"
+								alt="user avatar"
+							/>
+							<div className={styles.content}>
+								<h4>
+									How to get first item on screen of a list view while scrolling
+								</h4>
+								<p>12 Replies</p>
+							</div>
+						</div>
+					</div>
+
+					{/* Accounts suggestions */}
+					<div className={`${styles.users} ${styles['aside-container']}`}>
+						<h3 className={styles['heading-secondary']}>You might follow</h3>
+
+						<div className={styles.user}>
+							<img
+								src="https://www.pngitem.com/pimgs/m/581-5813504_avatar-dummy-png-transparent-png.png"
+								alt="user avatar"
+							/>
+							<div className={styles.details}>
+								<div>
+									<h4>adebami_dev</h4>
+									<p>@adebami_dev</p>
+								</div>
+								<p>Follows you</p>
+							</div>
+							<button type="button">Follow</button>
+						</div>
+						<div className={styles.user}>
+							<img
+								src="https://www.pngitem.com/pimgs/m/581-5813504_avatar-dummy-png-transparent-png.png"
+								alt="user avatar"
+							/>
+							<div className={styles.details}>
+								<div>
+									<h4>adebami_dev</h4>
+									<p>@adebami_dev</p>
+								</div>
+								<p>Follows you</p>
+							</div>
+							<button type="button">Follow</button>
+						</div>
+						<div className={styles.user}>
+							<img
+								src="https://www.pngitem.com/pimgs/m/581-5813504_avatar-dummy-png-transparent-png.png"
+								alt="user avatar"
+							/>
+							<div className={styles.details}>
+								<div>
+									<h4>adebami_dev</h4>
+									<p>@adebami_dev</p>
+								</div>
+								<p>Follows you</p>
+							</div>
+							<button type="button">Follow</button>
+						</div>
+
+						<p>See more</p>
 					</div>
 				</section>
 			</aside>
