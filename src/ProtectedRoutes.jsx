a1957586262029c5e1b4ddf094d186f86e5fe656
BamBam-sys <lawalayobamidavid@gmail.com>
2022-12-14 12:06:04 +0100

    route to login page it not authenticated

--- src/ProtectedRoutes.jsx
+++ src/ProtectedRoutes.jsx
@@ -4,7 +4,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 function ProtectedRoutes() {
 	const token = localStorage.getItem('token');
 
-	return token ? <Outlet /> : <Navigate to="/" />;
+	return token ? <Outlet /> : <Navigate to="/login" />;
 }
 
 export default ProtectedRoutes;
