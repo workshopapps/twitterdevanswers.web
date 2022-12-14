1a6e9e9f40b8477aa78e9e36d84bce433d5d2e04
Nnaemeka Joseph <elomnnaemekaj@gmail.com>
2022-12-14 15:10:41 +0100

    2fa and change password fixed

--- src/pages/Security/Security.jsx
+++ src/pages/Security/Security.jsx
@@ -1,102 +1,138 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import qrcode from "qrcode";
 import styles from "./Security.module.css";
 
 function Security() {
     const [getEmail, setGetEmail] = useState("email");
+    const [fetchEmail, setFetchEmail] = useState("email");
     const [postEmail, setPostEmail] = useState("email");
     const [OTP, setOTP] = useState("Token");
     const [isVisible, setIsVisible] = useState(false);
     const [response, setResponse] = useState("");
+    const [verifyResponse, setVerifyResponse] = useState("");
+    const [resetResponse, setResetResponse] = useState("");
     const [mydata, setMyData] = useState("");
     const [code, setCode] = useState(false);
-    const [button, setButton] = useState(false);
     const [buttons, setButtons] = useState(false);
     const [verifyToken, setVerifyToken] = useState(false);
     const [oldPassword, setOldPassword] = useState("**************");
     const [newPassword, setNewPassword] = useState("**************");
     const [confirmPassword, setConfirmPassword] = useState("**************");
     const [hideReset, setHideReset] = useState(false);
+    const [isSetUpVisible, setIsSetUpVisible] = useState(false);
 
 
     function toggleForm(){
         setIsVisible(!isVisible);
-        setVerifyToken(false)
-        setButton(!button)
-        
+        setVerifyToken(false);
+        setIsSetUpVisible(false)
+    }
+    function toggleSetUpForm(){
+        setIsSetUpVisible(!isSetUpVisible);
+        setIsVisible(false);
+        setVerifyToken(false);
     }
     function toggleButtons (){
-        setButtons(!buttons)
+        setButtons(!buttons);
     }
     function showVerifyForm(){
-        setVerifyToken(!verifyToken)
-        setButton(false)
-        setCode(false)
-        setIsVisible(false)
+        setVerifyToken(!verifyToken);
+        setCode(false);
+        setIsVisible(false);
+        setIsSetUpVisible(false)
     }
     function revealCode(){
-        if (mydata){
-            setCode(true);
-        }
-
+            setCode(!code);
     }
     function hideResetForm(){
         setHideReset(!hideReset);
     }
     
-        const handleSubmit = async (e) => {
+    const token = localStorage.getItem("token");
+
+        const handleSetUpMFA = async (e) => {
             e.preventDefault();
             try {
                 const { data } = await axios.put('https://api.devask.hng.tech/auth/setup-mfa', {
                     "email": getEmail
-                });
-                     qrcode.toDataURL(data.code, {with: 250}, (error, newdata) => {
-                        if (error) throw error
-                        if (newdata) {
-                            setMyData(newdata);
-                        }  
-                    })
+                },
+                { 
+                    headers:{
+                        'Authorization': `Bearer ${token}`
+                }
+            });
                 if(data){
-                    setResponse(data.message);
+                    setResponse(data.detail);
                 };
                 
             } catch (error) {
                 setResponse("Could not send request. Please try again!");
             }
         }
+
+        const handleGenerateCode = async (e) => {
+            e.preventDefault();
+            try {
+                const { data } = await axios.post('https://api.devask.hng.tech/auth/send-mfa', {
+                    "email": fetchEmail
+                },
+                { 
+                    headers:{
+                        'Authorization': `Bearer ${token}`
+                }
+            });
+                if(data){
+                    setMyData(data.qr_code);
+                };
+                
+            } catch (error) {
+                setResponse("Could not send request. Please try again!");
+            }
+        }
+
              const handleSubmitOTP = async (e) => {
                 e.preventDefault();
                 try {
                     const { data } = await axios.post('https://api.devask.hng.tech/auth/validate-mfa', {
                             "email": postEmail,
                             "mfa_hash": OTP
-                    });
+                    },
+                    { 
+                        headers:{
+                            'Authorization': `Bearer ${token}`
+                    }
+                });
                     if(data){
-                        setResponse(data);
+                        setVerifyResponse("Validation Successful");
                         
                     }; 
                 } catch (error) {
-                    setResponse("Validation Unsuccessful. Please try again!");   
+                    setVerifyResponse("Validation Unsuccessful. Please try again!");   
                 } 
             }
-
+            
             const handleReset = async (e) => {
                 e.preventDefault();
                 try {
-                    const { data } = await axios.put('https://api.devask.hng.tech/auth/change-password', {
-                        "oldPassword": oldPassword,
-                        "newPassword": newPassword,
-                        "confirmPassword": confirmPassword
+                    
+                    const { data } = await axios.put('https://api.devask.hng.tech/auth/change-password', 
+                    {    
+                            "oldPassword": oldPassword,
+                            "newPassword": newPassword,
+                            "confirmPassword": confirmPassword,       
+                    },
+                    { 
+                        headers:{
+                            'Authorization': `Bearer ${token}`
+                    }
                     });
                     if(data){
-                        setResponse(data.message);
+                        setResetResponse(data.message);
                     };
                     
                 } catch (error) {
-                    setResponse("Password Reset Unsuccessful. Please try again!");
-                    console.log(error)
+                    setResetResponse("Password Reset Unsuccessful. Please try again!");
                 }
             }
 
@@ -151,30 +187,39 @@ function Security() {
                 <div className={styles.TwoFactorAuthForms} id="two-factor-forms">
                     { buttons &&
                     <div>
-                <button type="submit" onClick={toggleForm}>Setup MFA</button>
+                <button type="submit" onClick={toggleSetUpForm}>Setup MFA</button>
+                <button type="submit" onClick={toggleForm}>Generate QR Code</button>
                 <button type="submit" onClick={showVerifyForm}>Verify Code</button>
                  </div>
                  }   
-                {isVisible && 
+                 {isSetUpVisible && 
                  <div>
                     <p>Please fill the required fields</p>
-                <form className={styles.GetTwoAuthForm} onSubmit={handleSubmit} method="POST">
+                <form className={styles.GetTwoAuthForm} onSubmit={handleSetUpMFA} method="POST">
                         <input type="email" name="email" required value={getEmail} onChange={(e) => setGetEmail(e.target.value)} />
-                        <button type="submit">Generate</button>
+                        <button type="submit">SetUp</button>
                     </form>
                     <p>{response}</p>
                        </div>
     } 
-                    {button &&
-                        <button type="submit" onClick={revealCode}>Display Code</button>
-                        
-                    }
-                    {code && <div>
+                {isVisible && 
+                 <div>
+                    <p>Please fill the required fields</p>
+                <form className={styles.GetTwoAuthForm} onSubmit={handleGenerateCode} method="POST">
+                        <input type="email" name="email" required value={fetchEmail} onChange={(e) => setFetchEmail(e.target.value)} />
+                        <button type="submit" onClick={revealCode}>Generate</button>
+                    </form>
+                    {code && 
+                    
+                                <div>
                                     <p>Please Scan the Code</p>
                                     <img src={mydata} alt="data" />
                                     </div>
+                    } 
+                       </div>
+                    } 
+    
                     
-                    }
                     { verifyToken &&
                     <div>
                     <form className={styles.GetTwoAuthForm} onSubmit={handleSubmitOTP} method="POST">
@@ -183,7 +228,7 @@ function Security() {
                         <button type="submit">Verify Token</button>
                     
                     </form>
-                            <p>{response}</p>
+                            <p>{verifyResponse}</p>
                 </div>
                 }
                 </div>
@@ -207,7 +252,7 @@ function Security() {
                         <button type="submit">Change Password</button>
                     
                     </form>
-                            <p>{response}</p>
+                            <p>{resetResponse}</p>
                 </div>
                  }
             </section>
